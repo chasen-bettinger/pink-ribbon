3acec01a431b0ed03443dd10ed899bf8fc054d3f
Chasen Bettinger <bettingerchasen@gmail.com>
2018-02-08 11:05:48 -0500

    Pink blog code is done

--- data-analysis.py
+++ data-analysis.py
@@ -5,6 +5,18 @@ from textblob import TextBlob
 
 df = pd.read_csv("Cleaned-Text.csv")
 
+df['post-id'], df['post-no'] = df['ID'].str.split('-', 1).str
+cols = df.columns.tolist()
+
+a, b = cols.index('post-id'), cols.index('Date')
+cols[b], cols[a] = cols[a], cols[b]
+
+a, b = cols.index('post-no'), cols.index('Post')
+cols[b], cols[a] = cols[a], cols[b]
+
+cols.remove('ID')
+
+df = df[cols]
 
 def checkPink(sentence):
     if 'pink' in sentence:
@@ -28,17 +40,42 @@ def analyze_sentiment(post):
     else:
         return -1
 
+# To see the raw data, retrieve the polarity
 
+def retrieve_polarity(post):
+
+    analysis = TextBlob(post)
+    return analysis.sentiment.polarity
+
+#######################################################
+#######################################################
+
+# Create a column evaluating if the post contains Pink
 df['contains_pink'] = df.Post.apply(lambda x: checkPink(x))
 
 # These are the target blogs we want to analyze
 pinkBlogs = df[df['contains_pink'] == 1]
+
+# This these ids are important because these people discuss pink in their blogs
+idsToAnalyze = []
+
+for index, row in pinkBlogs.iterrows():
+    if row['post-id'] not in idsToAnalyze:
+        idsToAnalyze.append(row['post-id'])
+
+
 
 # These are the blogs that will serve as our baseline
 # nonPinkBlogs = df[df['contains_pink'] == 0]
 
 pinkBlogs['SA'] = np.array([analyze_sentiment(post)
                             for post in pinkBlogs['Post']])
+
+pinkBlogs['Polarity'] = [retrieve_polarity(post) for post in pinkBlogs['Post']]
+
+
+
+pinkBlogs.to_csv("Pink-Blogs.csv")
 
 pos_thoughts = 0
 neu_thoughts = 0
@@ -52,6 +89,7 @@ for index, row in pinkBlogs.iterrows():
     if row['SA'] < 0:
         neg_thoughts += 1
 
-y = len(pinkBlogs('Post'))
-print(y)
-# print(pinkBlogs)
+print("Percentage of positive posts: {}%".format(pos_thoughts*100/len(pinkBlogs['Post'])))
+print("Percentage of neutral posts: {}%".format(neu_thoughts*100/len(pinkBlogs['Post'])))
+print("Percentage of negative posts: {}%".format(neg_thoughts*100/len(pinkBlogs['Post'])))
+
