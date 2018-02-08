e80dbe7f973a6ec9c4ebaaa3af275fd388a80ff3
chasen-bettinger <bettingerchasen@gmail.com>
2018-02-08 09:33:12 -0500

    Uploading all code that I have, still working on pinkblogs

--- data-analysis.py
+++ data-analysis.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from textblob import TextBlob
+
 
 df = pd.read_csv("Cleaned-Text.csv")
 
@@ -10,6 +12,22 @@ def checkPink(sentence):
     else:
         return 0
 
+# Code retrieved from https://dev.to/rodolfoferro/sentiment-analysis-on-trumpss-tweets-using-python-
+
+
+def analyze_sentiment(post):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(post)
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
 
 df['contains_pink'] = df.Post.apply(lambda x: checkPink(x))
 
@@ -17,11 +35,23 @@ df['contains_pink'] = df.Post.apply(lambda x: checkPink(x))
 pinkBlogs = df[df['contains_pink'] == 1]
 
 # These are the blogs that will serve as our baseline
-nonPinkBlogs = df[df['contains_pink'] == 0]
+# nonPinkBlogs = df[df['contains_pink'] == 0]
 
-pinkIDs = []
+pinkBlogs['SA'] = np.array([analyze_sentiment(post)
+                            for post in pinkBlogs['Post']])
+
+pos_thoughts = 0
+neu_thoughts = 0
+neg_thoughts = 0
 
 for index, row in pinkBlogs.iterrows():
-    pinkIDs.append(row['ID'])
+    if row['SA'] > 0:
+        pos_thoughts += 1
+    if row['SA'] == 0:
+        neu_thoughts += 1
+    if row['SA'] < 0:
+        neg_thoughts += 1
 
-print(pinkIDs)
+y = len(pinkBlogs('Post'))
+print(y)
+# print(pinkBlogs)
